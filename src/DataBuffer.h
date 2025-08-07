c457f21514f48dc99e8bb52742d82abdd53e901e
hyird <hyird@outlook.com>
2025-08-07 16:13:59 +0800

    Update DataBuffer.h

--- src/DataBuffer.h
+++ src/DataBuffer.h
@@ -1,46 +1,35 @@
-#pragma once
-#include <unordered_map>
-#include <shared_mutex>
-#include <any>
-#include <chrono>
-#include <string>
-#include <optional>
-#include "Variable.h"
+#include "DataBuffer.h"
 
-class DataBuffer {
-public:
-    struct Entry {
-        std::any value{};
-        std::chrono::system_clock::time_point timestamp{};
-        VarQuality quality = VarQuality::UNCERTAIN;
+DataBuffer& DataBuffer::instance() {
+    static DataBuffer buf;
+    return buf;
+}
 
-        Entry() = default;
-        Entry(const std::any& v,
-              const std::chrono::system_clock::time_point& t,
-              VarQuality q)
-            : value(v), timestamp(t), quality(q) {}
-    };
+void DataBuffer::set(const std::string& varid,
+                     const std::any& value,
+                     const std::chrono::system_clock::time_point timestamp,
+                     const VarQuality quality) {
+    std::unique_lock lock(mtx_);
+    buffer_[varid] = Entry(value, timestamp, quality);
+}
 
-    static DataBuffer& instance();
+std::optional<std::any> DataBuffer::get(const std::string& varid) const {
+    std::shared_lock lock(mtx_);
+    if (const auto it = buffer_.find(varid); it != buffer_.end()) {
+        return it->second.value;
+    }
+    return std::nullopt;
+}
 
-    DataBuffer(const DataBuffer&) = delete;
-    DataBuffer& operator=(const DataBuffer&) = delete;
-    ~DataBuffer() = default;
+std::optional<DataBuffer::Entry> DataBuffer::getEntry(const std::string& varid) const {
+    std::shared_lock lock(mtx_);
+    if (const auto it = buffer_.find(varid); it != buffer_.end()) {
+        return it->second;
+    }
+    return std::nullopt;
+}
 
-    void set(const std::string& varid,
-             const std::any& value,
-             std::chrono::system_clock::time_point timestamp,
-             VarQuality quality);
-
-    std::optional<std::any> get(const std::string& varid) const;
-
-    std::optional<Entry> getEntry(const std::string& varid) const;
-
-    void remove(const std::string& varid);
-
-private:
-    explicit DataBuffer() = default;
-
-    std::unordered_map<std::string, Entry> buffer_;
-    mutable std::shared_mutex mtx_;
-};
+void DataBuffer::remove(const std::string& varid) {
+    std::unique_lock lock(mtx_);
+    buffer_.erase(varid);
+}
